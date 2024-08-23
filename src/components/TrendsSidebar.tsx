aacde9c26a0844606122205489c2c2c9fd3c61ec
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-08-23 21:51:01 +0530

    React Query Introduction

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -11,7 +11,7 @@ import { formatNumber } from "@/lib/utils";
 
 export default function TrendsSidebar() {
   return (
-    <div className="sticky top-[5.25rem] h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+    <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
         <TrendingTopics />
